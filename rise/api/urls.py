422083f541ffa32b883a04aae18e76adf36d85ef
David Galiev <galiev.david@yandex.ru>
2024-03-26 23:22:51 +0500

    Rewrite api from template form into json

--- rise/api/urls.py
+++ rise/api/urls.py
@@ -5,8 +5,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/", auth_views.LoginView.as_view(authentication_form=views.UserLoginForm), name="login_user"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout_user"),
     path("register/", views.register_user, name="register_user"),
-    path("profile/", views.user_profile, name="profile"),
+    path("login/", views.login_user, name="login_user"),
+    path("user/", views.get_user, name="get_user"),
+    path("logout/", views.logout_user, name="logout_user"),
+    path("delete_user/", views.delete_user, name="delete_user"),
 ]
--- rise/api/views.py
+++ rise/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from api.models import CustomUser
 from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpRequest
 from django.template import loader
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
@@ -11,64 +11,116 @@ def index(request):
     return HttpResponse("Hello, world. You're at the api index.")
 
 # Auth
-class UserLoginForm(AuthenticationForm):
-    def __init__(self, request, *args, **kwargs):
-        super().__init__(request, *args, **kwargs)
-        self.fields['username'].label = "Логин"
-        self.fields['password'].label = "Пароль"
-
-class UserRegisterForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].label = "Логин"
-        self.fields['password1'].label = "Пароль"
-        self.fields['password2'].label = "Подтвердите пароль"
-    
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'password1', 'password2')
-
 @csrf_exempt
-def register_user(request):
-    if request.method == 'GET':
-        # Render the register template
-        template = loader.get_template("registration/register.html")
-        context = {
-            'form': UserRegisterForm()
-        }
-        return HttpResponse(template.render(context, request))
+def register_user(request:HttpRequest):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            # Create user from form data
-            form.save(commit=False)
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = CustomUser.objects.create_user(username=username, password=password)
-            # Redirect user to login
-            return redirect('login_user')
-        else:
-            # Render the register template with error
-            template = loader.get_template("registration/register.html")
-            context = {
-                'form': form
-            }
-            return HttpResponse(template.render(context, request))
-    # Return an error response for unsupported request method
-    return JsonResponse({"error": "Invalid request method."}, status=405)
+        # Get the user data from the request
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Create a new user object
+        user = CustomUser.objects.create_user(username=username, password=password)
+
+        # Return the user ID in the response
+        return JsonResponse({"message": "User registered successfully", "user_id": user.id})
+    else:
+        # Return an error response for unsupported request method
+        return JsonResponse({"error": "Invalid request method."}, status=405)
 
 @csrf_exempt
-def user_profile(request):
-    if request.method == 'GET':
-        if request.user.is_authenticated:
-            # Render the register template
-            template = loader.get_template("user/profile.html")
-            context = {
-                'user': request.user
+def login_user(request:HttpRequest):
+    if request.method == 'POST':
+        # Check if user is not logged in yet
+        session_user_id = request.session.get('user_id')
+        if session_user_id is not None:
+            return JsonResponse({"error": "Cannot log into multiple users"}, status=403)
+
+        # Get the user data from the request
+        username = request.POST.get('username')
+
+        try:
+            # Find matching user
+            user = CustomUser.objects.get(username=username)
+            password = request.POST.get('password')
+
+            # Login if passwords match
+            if user.check_password(password):
+                request.session['user_id'] = user.id
+                return JsonResponse({"message": "Login successful"})
+            else:
+                return JsonResponse({"error": "Username or password not matching"}, status=406)
+        except CustomUser.DoesNotExist:
+            # Return an error response for a user that does not exist
+            return JsonResponse({"error": "User does not exist"}, status=404)
+    else:
+        # Return an error response for unsupported request method
+        return JsonResponse({"error": "Invalid request method."}, status=405)
+
+@csrf_exempt
+def get_user(request:HttpRequest):
+    if request.method == 'POST':
+        # Get the user data from the request
+        username = request.POST.get('username')
+
+        try:
+            # Find matching user
+            user = CustomUser.objects.get(username=username)
+
+            # Construct response
+            response = {
+                "message": "User found successfully",
+                "user_id": user.id,
+                "username": user.username
             }
-            return HttpResponse(template.render(context, request))
+
+            # If the target user is logged in, show additional info
+            session_user_id = request.session.get('user_id')
+            if (session_user_id is not None) and (user.id == session_user_id):
+                response['email'] = user.email
+
+            return JsonResponse(response)
+        except CustomUser.DoesNotExist:
+            # Return an error response for a user that does not exist
+            return JsonResponse({"error": "User does not exist"}, status=404)
+    else:
+        # Return an error response for unsupported request method
+        return JsonResponse({"error": "Invalid request method."}, status=405)
+
+@csrf_exempt
+def logout_user(request:HttpRequest):
+    if request.method == 'POST':
+        session_user_id = request.session.get('user_id')
+        if session_user_id is not None:
+            # Log out if logged in
+            request.session['user_id'] = None
+            return JsonResponse({"message": "Logout successful"})
         else:
-            # Redirect to login
-            return redirect('login_user')
-    # Return an error response for unsupported request method
-    return JsonResponse({"error": "Invalid request method."}, status=405)
+            # Return an error response for user that is not logged in yet
+            return JsonResponse({"error": "User not logged in"}, status=401)
+    else:
+        # Return an error response for unsupported request method
+        return JsonResponse({"error": "Invalid request method."}, status=405)
+
+@csrf_exempt
+def delete_user(request:HttpRequest):
+    if request.method == 'POST':
+        # Get the user data from the request
+        username = request.POST.get('username')
+
+        try:
+            # Find matching user
+            user = CustomUser.objects.get(username=username)
+            password = request.POST.get('password')
+
+            # Delete if passwords match
+            if user.check_password(password):
+                user.delete()
+                return JsonResponse({"message": "User deleted successfully"})
+            else:
+                return JsonResponse({"error": "Username or password not matching"}, status=406)
+        except CustomUser.DoesNotExist:
+            # Return an error response for a user that does not exist
+            return JsonResponse({"error": "User does not exist"}, status=404)
+    else:
+        # Return an error response for unsupported request method
+        return JsonResponse({"error": "Invalid request method."}, status=405)
