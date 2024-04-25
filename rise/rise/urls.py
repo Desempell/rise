8a47b21bb9c2fcadbe9e4648a3ebf535e58c4bf0
venberrry <elvenbe@gmail.com>
2024-04-25 16:11:42 +0300

    added swagger-skelet

--- rise/rise/urls.py
+++ rise/rise/urls.py
@@ -14,11 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from .yasg import urlpatterns as doc
 
 urlpatterns = [
-    path("api/", include("api.urls")),
     path('admin/', admin.site.urls),
-
+    path('api-auth/', include('rest_framework.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('api/v1/', include('api.urls')),
 ]
+
+urlpatterns += doc
